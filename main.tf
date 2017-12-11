95825deff0e86504035b094b3efc1c910c8e620d
Greg Chapman <Greg.Chapman@rackspace.com>
2017-12-11 12:44:02 -0600

    update with ec2 instance

--- main.tf
+++ main.tf
@@ -11,10 +11,19 @@ terraform {
   }
 }
 
+# Do not rename a module after creating else the entire resource will be rebuilt
+# Original work might be destroyed
 module "Base-Newtork-GBC" {
 	source ="./base-network"
 	environment="Martian Prod"
 	name = "Prod-Martian-subnets-Ticket-1111"
 	public_subnets = ["172.18.0.0/22", "172.18.4.0/22"]
 	private_subnets = ["172.18.32.0/21", "172.18.40.0/21"]
+}
+
+resource "aws_instance" "First-EC2-server-public" {
+	ami = "ami-55ef662f"
+	instance_type = "t2.micro"
+	tags = { name = "First-Instance" }
+	subnet_id = "${module.Base-Newtork-GBC.public_subnets[0]}"
 }
